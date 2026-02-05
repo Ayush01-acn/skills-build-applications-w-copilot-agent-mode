5781f7f97fcef5b818591826554c29c70e5e461f
Ayush01-acn <ayush.b.chaudhary@accenture.com>
2026-02-05 08:25:54 +0000

    v0.11

--- octofit-tracker/frontend/src/App.js
+++ octofit-tracker/frontend/src/App.js
@@ -1,6 +1,6 @@
 
 
-import octofitLogo from '../public/octofitapp-small.png';
+
 
 
 function App() {
@@ -9,7 +9,7 @@ function App() {
       <nav className="navbar navbar-expand-lg navbar-dark bg-primary mb-4">
         <div className="container-fluid">
           <Link className="navbar-brand fw-bold d-flex align-items-center" to="/">
-            <img src={octofitLogo} alt="Octofit Logo" className="octofit-logo" />
+            <img src={process.env.PUBLIC_URL + '/octofitapp-small.png'} alt="Octofit Logo" className="octofit-logo" />
             Octofit Tracker
           </Link>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
